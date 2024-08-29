aa5ae020d38fe981bcee063e4fbf15631032234a
abfar481 <abifarabi4881@gmail.com>
2024-08-30 00:58:02 +0700

    adding function to playround for 1 round(first step)

--- main.js
+++ main.js
@@ -2,6 +2,32 @@
 let humanScore = 0;
 let computerScore = 0;
 
+//Function for play around
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log(`It's a Tie! Both chose ${humanChoice}.`);
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")
+     ) {
+        humanScore++;
+        console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`);
+     } else {
+        computerScore++;
+        console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.`);
+     }
+
+     console.log(`Score: You - ${humanScore}, Computer - ${computerScore}`);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
 //Function computer choices
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
